58096ba74cffe2eddd1ab585e0089563963ef34c
Sandhy <tiwarisandhyprakash@gmail.com>
2023-02-18 20:45:15 +0530

    Second commit

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -27,6 +27,23 @@ public class Workspace extends Gmail{
 //         1. At a particular time, you can be present in at most one meeting
 //         2. If you want to attend a meeting, you must join it at its start time and leave at end time.
 //         Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-        return 3;
+            double [][]t=new double[calendar.size()][2];
+            for(int i=0;i< calendar.size();i++){
+                String st=calendar.get(i).getStartTime().toString();
+                String se=calendar.get(i).getEndTime().toString();
+                t[i][0]=Double.valueOf(st.substring(0,2))+Double.valueOf(st.substring(3))/100;
+                //System.out.println(t[i][0]);
+                t[i][1]=Double.valueOf(se.substring(0,2))+Double.valueOf(se.substring(3))/100;
+            }
+            Arrays.sort(t,(a,b)->Double.compare(a[1],b[1]));
+           int c=0;
+           Double pe=Double.MAX_VALUE;
+           for(int i=0;i<t.length;i++){
+               if(t[i][0]<pe){
+                   c++;
+                   pe=t[i][1];
+               }
+           }
+           return c;
          }
     }
